b7a40345088a3fd02f979f8d40ca2c7c9cd7727e
Lucas Su <lijie.su93@gmail.com>
2024-03-12 14:13:47 +1030

    add: implete comment section

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,66 @@
+import Comments from "@/components/Comments";
+import Menu from "@/components/Menu";
+import Image from "next/image";
+
+const SinglePage = () => {
+  return (
+    <div className="container">
+      <div className="row" style={{ height: "350px" }}>
+        <div className="col d-flex flex-column">
+          <h1 className="fs-0 fw-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <div className="mt-3 d-flex">
+            <div className="m-0 ">
+              <Image
+                className="rounded-circle"
+                src="/p1.jpeg"
+                width={55}
+                height={55}
+                alt=""
+              ></Image>
+            </div>
+            <div className="ms-3  d-flex align-items-start justify-content-center flex-column">
+              <p className="fs-7  m-0">John Doe</p>
+              <p className="fs-7 text-soft m-0 ">01.09.2023</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="col position-relative"
+          style={{ width: "100%", height: "100%", overflow: "hidden" }}
+        >
+          <Image
+            src="/p1.jpeg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            className="position-absolute top-0 start-0 rounded"
+          ></Image>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-8  mt-5 pe-4">
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Perferendis, distinctio unde dolores in obcaecati velit voluptas
+            corrupti, reprehenderit quibusdam beatae eum impedit aspernatur, ab
+            vitae quas rerum minima? Dolor, iste id ex labore aliquid nesciunt
+            ab nemo blanditiis vel quibusdam. Commodi ratione voluptatum quas
+            quaerat voluptate provident numquam alias nesciunt rerum pariatur
+            tempore ducimus possimus architecto est sapiente, libero rem fugiat
+            necessitatibus exercitationem repudiandae autem odio minus? Dolorem
+            laudantium eos facilis doloribus fugit, quo porro corrupti pariatur.
+            Sit pariatur, cumque nulla laudantium, perferendis, officia rem odit
+            eum fugiat reprehenderit inventore voluptate doloremque fuga
+            blanditiis dolor asperiores quidem suscipit nemo harum?
+          </div>
+          <Comments />
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+export default SinglePage;
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Comments = () => {
@@ -10,8 +11,87 @@ const Comments = () => {
     );
   }
   return (
-    <div className="container">
-      <h1>Comments</h1>
+    <div className="mt-5">
+      <h3>Comments</h3>
+      <div>
+        <textarea
+          class="form-control mt-3"
+          id="exampleFormControlTextarea1"
+          placeholder="write a comment..."
+          rows="5"
+        ></textarea>
+        <button type="button" class="btn btn-success mt-3">
+          Send
+        </button>
+      </div>
+      <div className="mt-5">
+        <div className="d-flex flex-column mt-3">
+          <div className="d-flex">
+            <div className="m-0 ">
+              <Image
+                className="rounded-circle"
+                src="/p1.jpeg"
+                width={40}
+                height={40}
+                alt=""
+              ></Image>
+            </div>
+            <div className="ms-3  d-flex align-items-start justify-content-center flex-column">
+              <p className="fs-7  m-0">John Doe</p>
+              <p className="fs-7 text-soft m-0 ">01.09.2023</p>
+            </div>
+          </div>
+          <p className="mt-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam
+            necessitatibus, quibusdam a ipsam quasi modi illo quis inventore
+            neque perspiciatis?
+          </p>
+        </div>
+        <div className="d-flex flex-column mt-3">
+          <div className="d-flex">
+            <div className="m-0 ">
+              <Image
+                className="rounded-circle"
+                src="/p1.jpeg"
+                width={40}
+                height={40}
+                alt=""
+              ></Image>
+            </div>
+            <div className="ms-3  d-flex align-items-start justify-content-center flex-column">
+              <p className="fs-7  m-0">John Doe</p>
+              <p className="fs-7 text-soft m-0 ">01.09.2023</p>
+            </div>
+          </div>
+          <p className="mt-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam
+            necessitatibus, quibusdam a ipsam quasi modi illo quis inventore
+            neque perspiciatis?
+          </p>
+        </div>
+        <div className="d-flex flex-column mt-3">
+          <div className="d-flex">
+            <div className="m-0 ">
+              <Image
+                className="rounded-circle"
+                src="/p1.jpeg"
+                width={40}
+                height={40}
+                alt=""
+              ></Image>
+            </div>
+            <div className="ms-3  d-flex align-items-start justify-content-center flex-column">
+              <p className="fs-7  m-0">John Doe</p>
+              <p className="fs-7 text-soft m-0 ">01.09.2023</p>
+            </div>
+          </div>
+          <p className="mt-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam
+            necessitatibus, quibusdam a ipsam quasi modi illo quis inventore
+            neque perspiciatis?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
